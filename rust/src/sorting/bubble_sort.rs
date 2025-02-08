d969dc840041dc83b49a2d68ff5a85c0fd3a8036
Jordan Roberts <jordanroberts333@icloud.com>
2025-02-08 22:24:46 +0000

    chor(rs): changed bubbleSort to accept vectors instead of arrays

--- rust/src/sorting/bubble_sort.rs
+++ rust/src/sorting/bubble_sort.rs
@@ -1,8 +1,10 @@
-pub fn bubble_sort<const N: usize>(arr: &[i32; N]) -> [i32; N] {
+pub fn bubble_sort(arr: &Vec<i32>) -> Vec<i32> {
     let mut cloned_arr = arr.clone();
-    for i in 0..cloned_arr.len() - 1 {
+    let len = cloned_arr.len();
+
+    for i in 0..len - 1 {
         let mut swapped = false;
-        for j in 0..cloned_arr.len() - i - 1 {
+        for j in 0..len - i - 1 {
             if cloned_arr[j + 1] < cloned_arr[j] {
                 cloned_arr.swap(j, j + 1);
                 swapped = true;
@@ -10,10 +12,10 @@ pub fn bubble_sort<const N: usize>(arr: &[i32; N]) -> [i32; N] {
         }
         if !swapped {
             break;
-        };
+        }
     }
 
-    return cloned_arr;
+    cloned_arr
 }
 
 #[cfg(test)]
@@ -22,22 +24,22 @@ mod tests {
 
     #[test]
     fn test_bubble_sort() {
-        let unsorted = [5, 2, 9, 1, 7, 6, 3, 8, 4, 0];
+        let unsorted = vec![5, 2, 9, 1, 7, 6, 3, 8, 4, 0];
         let sorted = bubble_sort(&unsorted);
-        assert_eq!(sorted, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(sorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 
     #[test]
     fn test_already_sorted() {
-        let sorted_input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let sorted_input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         let result = bubble_sort(&sorted_input);
         assert_eq!(result, sorted_input);
     }
 
     #[test]
     fn test_reverse_sorted() {
-        let reverse_sorted = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        let reverse_sorted = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
         let sorted = bubble_sort(&reverse_sorted);
-        assert_eq!(sorted, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(sorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
