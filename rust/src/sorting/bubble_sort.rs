80d8b8230377fa2a02c06c9c70e380b158f430d7
Jordan Roberts <jordanroberts333@icloud.com>
2025-02-09 22:16:51 +0000

    chor(rs): bubbleSort sorts in place

--- rust/src/sorting/bubble_sort.rs
+++ rust/src/sorting/bubble_sort.rs
@@ -1,12 +1,9 @@
-pub fn bubble_sort(arr: &Vec<i32>) -> Vec<i32> {
-    let mut cloned_arr = arr.clone();
-    let len = cloned_arr.len();
-
-    for i in 0..len - 1 {
+pub fn bubble_sort(arr: &mut Vec<i32>) {
+    for i in 0..arr.len() - 1 {
         let mut swapped = false;
-        for j in 0..len - i - 1 {
-            if cloned_arr[j + 1] < cloned_arr[j] {
-                cloned_arr.swap(j, j + 1);
+        for j in 0..arr.len() - i - 1 {
+            if arr[j + 1] < arr[j] {
+                arr.swap(j, j + 1);
                 swapped = true;
             }
         }
@@ -14,8 +11,6 @@ pub fn bubble_sort(arr: &Vec<i32>) -> Vec<i32> {
             break;
         }
     }
-
-    cloned_arr
 }
 
 #[cfg(test)]
@@ -24,22 +19,22 @@ mod tests {
 
     #[test]
     fn test_bubble_sort() {
-        let unsorted = vec![5, 2, 9, 1, 7, 6, 3, 8, 4, 0];
-        let sorted = bubble_sort(&unsorted);
-        assert_eq!(sorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        let mut unsorted = vec![5, 2, 9, 1, 7, 6, 3, 8, 4, 0];
+        bubble_sort(&mut unsorted);
+        assert_eq!(unsorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 
     #[test]
     fn test_already_sorted() {
-        let sorted_input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let result = bubble_sort(&sorted_input);
-        assert_eq!(result, sorted_input);
+        let mut sorted_input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        bubble_sort(&mut sorted_input);
+        assert_eq!(sorted_input, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 
     #[test]
     fn test_reverse_sorted() {
-        let reverse_sorted = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-        let sorted = bubble_sort(&reverse_sorted);
-        assert_eq!(sorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        let mut reverse_sorted = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        bubble_sort(&mut reverse_sorted);
+        assert_eq!(reverse_sorted, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
